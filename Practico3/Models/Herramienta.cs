7440750e38878dd4cf081f1f9a055d8cc7bdc3f7
Danirock666 <jorgepardo642@gmail.com>
2024-10-23 22:16:06 -0300

    arreglo de modelos#

--- Practico3/Models/Herramienta.cs
+++ Practico3/Models/Herramienta.cs
@@ -3,7 +3,7 @@
     public class Herramienta
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Name { get; set; }
         public string NumeroSerie { get; set; }
         public string Estado  { get; set; }
     
--- Practico3/Models/Usuario.cs
+++ Practico3/Models/Usuario.cs
@@ -3,7 +3,7 @@
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Name { get; set; }
 
     }
 }
