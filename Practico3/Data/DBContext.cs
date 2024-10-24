e71d9698f62d6368f89a7ebb199701973fdaee2f
vicentetorresr <vicentetorres2c@gmail.com>
2024-10-23 21:12:21 -0300

    configuracion basica del proyecto. (DBContext, builder, appsettings)

---
+++ Practico3/Data/DBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Practico3.Data
+{
+    public class Contextt : DbContext
+    {
+        public Contextt(DbContextOptions<Contextt> options) : base(options)
+        {
+        }
+
+        //public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Datos iniciales para Usuarios
+            /*modelBuilder.Entity<Usuario>().HasData(new Usuario
+            {
+                Id = 1,
+                Name = "Vicente",
+                Fecha = DateTime.Now,
+            });
+            */
+        }
+    }
+}
