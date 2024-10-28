82b06fc851fa5fac634c3b37c0054e63a3a38a79
angelo23cft <angmaureira23@cftsa.cl>
2024-10-28 01:09:21 -0300

    Nuevo modelo asignacion creado

---
+++ Practico3/Models/Asignacion.cs
@@ -0,0 +1,14 @@
+﻿namespace Practico3.Models
+{
+    public class Asignacion
+    {
+        public int Id { get; set; }
+        public int HerramientaId { get; set; }
+        public Herramienta Herramienta { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime FechaAsignacion { get; set; }
+        public DateTime? FechaDevolucion { get; set; }
+        public string Estado { get; set; } // 'Asignado', 'En Mantenimiento', etc.
+    }
+}
