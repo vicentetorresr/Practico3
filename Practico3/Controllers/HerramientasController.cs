57297f0063358f9d72df317b62124f822aec9d96
angelo23cft <angmaureira23@cftsa.cl>
2024-10-28 03:06:34 -0300

    creacion de controlador y vista de herramienta y ingresoherramientas

---
+++ Practico3/Controllers/HerramientasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Practico3.Data;
+using Practico3.Models;
+
+namespace Practico3.Controllers
+{
+    public class HerramientasController : Controller
+    {
+        private readonly Contextt _context;
+
+        public HerramientasController(Contextt context)
+        {
+            _context = context;
+        }
+
+        // GET: Herramientas
+        public async Task<IActionResult> Index()
+        {
+            var contextt = _context.Herramientas.Include(h => h.Marca);
+            return View(await contextt.ToListAsync());
+        }
+
+        // GET: Herramientas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Herramientas == null)
+            {
+                return NotFound();
+            }
+
+            var herramienta = await _context.Herramientas
+                .Include(h => h.Marca)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (herramienta == null)
+            {
+                return NotFound();
+            }
+
+            return View(herramienta);
+        }
+
+        // GET: Herramientas/Create
+        public IActionResult Create()
+        {
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Id");
+            return View();
+        }
+
+        // POST: Herramientas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Modelo,MarcaId,CantidadTotal,CantidadDisponible,CantidadUsada,CantidadEnMantenimiento,Estado")] Herramienta herramienta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(herramienta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Id", herramienta.MarcaId);
+            return View(herramienta);
+        }
+
+        // GET: Herramientas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Herramientas == null)
+            {
+                return NotFound();
+            }
+
+            var herramienta = await _context.Herramientas.FindAsync(id);
+            if (herramienta == null)
+            {
+                return NotFound();
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Id", herramienta.MarcaId);
+            return View(herramienta);
+        }
+
+        // POST: Herramientas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Modelo,MarcaId,CantidadTotal,CantidadDisponible,CantidadUsada,CantidadEnMantenimiento,Estado")] Herramienta herramienta)
+        {
+            if (id != herramienta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(herramienta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HerramientaExists(herramienta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Id", herramienta.MarcaId);
+            return View(herramienta);
+        }
+
+        // GET: Herramientas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Herramientas == null)
+            {
+                return NotFound();
+            }
+
+            var herramienta = await _context.Herramientas
+                .Include(h => h.Marca)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (herramienta == null)
+            {
+                return NotFound();
+            }
+
+            return View(herramienta);
+        }
+
+        // POST: Herramientas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Herramientas == null)
+            {
+                return Problem("Entity set 'Contextt.Herramientas'  is null.");
+            }
+            var herramienta = await _context.Herramientas.FindAsync(id);
+            if (herramienta != null)
+            {
+                _context.Herramientas.Remove(herramienta);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HerramientaExists(int id)
+        {
+          return (_context.Herramientas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Practico3/Controllers/IngresoHerramientasController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Practico3.Data;
+using Practico3.Models;
+
+namespace Practico3.Controllers
+{
+    public class IngresoHerramientasController : Controller
+    {
+        private readonly Contextt _context;
+
+        public IngresoHerramientasController(Contextt context)
+        {
+            _context = context;
+        }
+
+        // GET: IngresoHerramientas
+        public async Task<IActionResult> Index()
+        {
+            var contextt = _context.IngresoHerramientas.Include(i => i.Herramienta).Include(i => i.Usuario);
+            return View(await contextt.ToListAsync());
+        }
+
+        // GET: IngresoHerramientas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.IngresoHerramientas == null)
+            {
+                return NotFound();
+            }
+
+            var ingresoHerramienta = await _context.IngresoHerramientas
+                .Include(i => i.Herramienta)
+                .Include(i => i.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingresoHerramienta == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingresoHerramienta);
+        }
+
+        // GET: IngresoHerramientas/Create
+        public IActionResult Create()
+        {
+            ViewData["HerramientaId"] = new SelectList(_context.Herramientas, "Id", "Id");
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id");
+            return View();
+        }
+
+        // POST: IngresoHerramientas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,HerramientaId,FechaIngreso,FechaDevolucion,EstaEnUso,UsuarioId")] IngresoHerramienta ingresoHerramienta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ingresoHerramienta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HerramientaId"] = new SelectList(_context.Herramientas, "Id", "Id", ingresoHerramienta.HerramientaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", ingresoHerramienta.UsuarioId);
+            return View(ingresoHerramienta);
+        }
+
+        // GET: IngresoHerramientas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.IngresoHerramientas == null)
+            {
+                return NotFound();
+            }
+
+            var ingresoHerramienta = await _context.IngresoHerramientas.FindAsync(id);
+            if (ingresoHerramienta == null)
+            {
+                return NotFound();
+            }
+            ViewData["HerramientaId"] = new SelectList(_context.Herramientas, "Id", "Id", ingresoHerramienta.HerramientaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", ingresoHerramienta.UsuarioId);
+            return View(ingresoHerramienta);
+        }
+
+        // POST: IngresoHerramientas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HerramientaId,FechaIngreso,FechaDevolucion,EstaEnUso,UsuarioId")] IngresoHerramienta ingresoHerramienta)
+        {
+            if (id != ingresoHerramienta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ingresoHerramienta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IngresoHerramientaExists(ingresoHerramienta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HerramientaId"] = new SelectList(_context.Herramientas, "Id", "Id", ingresoHerramienta.HerramientaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", ingresoHerramienta.UsuarioId);
+            return View(ingresoHerramienta);
+        }
+
+        // GET: IngresoHerramientas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.IngresoHerramientas == null)
+            {
+                return NotFound();
+            }
+
+            var ingresoHerramienta = await _context.IngresoHerramientas
+                .Include(i => i.Herramienta)
+                .Include(i => i.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingresoHerramienta == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingresoHerramienta);
+        }
+
+        // POST: IngresoHerramientas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.IngresoHerramientas == null)
+            {
+                return Problem("Entity set 'Contextt.IngresoHerramientas'  is null.");
+            }
+            var ingresoHerramienta = await _context.IngresoHerramientas.FindAsync(id);
+            if (ingresoHerramienta != null)
+            {
+                _context.IngresoHerramientas.Remove(ingresoHerramienta);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IngresoHerramientaExists(int id)
+        {
+          return (_context.IngresoHerramientas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
