283af97f1d30c7deb2a735ed09ef9f723fa57f34
angelo23cft <angmaureira23@cftsa.cl>
2024-10-28 01:07:34 -0300

    Cambio en modelos marca y usuario

--- Practico3/Models/Marca.cs
+++ Practico3/Models/Marca.cs
@@ -3,6 +3,7 @@
     public class Marca
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Nombre { get; set; }
+        public List<Herramienta> Herramientas { get; set; } // Relación con herramientas de esta marca
     }
 }
--- Practico3/Models/Usuario.cs
+++ Practico3/Models/Usuario.cs
@@ -3,7 +3,9 @@
     public class Usuario
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int HerramientasAsignadas { get; set; } // Máximo permitido: 3
+        public List<Asignacion> Asignaciones { get; set; } // Relación con asignaciones de herramientas
     }
 }
