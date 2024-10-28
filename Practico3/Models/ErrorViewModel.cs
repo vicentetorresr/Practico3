a671f4ea48bfa7119317196e7f6731b18ab81b37
angelo23cft <angmaureira23@cftsa.cl>
2024-10-28 01:05:39 -0300

    Cambios corregir modelos errores en error viewmodel y herramienta

--- Practico3/Models/ErrorViewModel.cs
+++ Practico3/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace Practico3.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
--- Practico3/Models/Herramienta.cs
+++ Practico3/Models/Herramienta.cs
@@ -3,9 +3,15 @@
     public class Herramienta
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string NumeroSerie { get; set; }
-        public string Estado  { get; set; }
-    
+        public string Modelo { get; set; }
+        public int MarcaId { get; set; } // Llave foránea para Marca
+        public Marca Marca { get; set; }
+        public int CantidadTotal { get; set; }
+        public int CantidadDisponible { get; set; }
+        public int CantidadUsada { get; set; }
+        public int CantidadEnMantenimiento { get; set; }
+        public string Estado { get; set; } // 'Disponible', 'En Uso', 'En Mantenimiento'
+        public List<Asignacion> Asignaciones { get; set; } // Relación con asignaciones
+        public List<Mantenimiento> Mantenimientos { get; set; } // Relación con mantenimientos
     }
 }
