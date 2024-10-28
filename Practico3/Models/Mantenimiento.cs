92c09addaffaeb262fd15917d75291939140db26
angelo23cft <angmaureira23@cftsa.cl>
2024-10-28 01:11:58 -0300

    arreglo de error en modelo ingresoherramienta y creacion de nuevo modelo mantenimiento

--- Practico3/Models/Mantenimiento.cs
+++ Practico3/Models/Mantenimiento.cs
@@ -1,14 +1,12 @@
 ﻿namespace Practico3.Models
 {
-    public class IngresoHerramienta
+    public class Mantenimiento
     {
         public int Id { get; set; }
         public int HerramientaId { get; set; }
         public Herramienta Herramienta { get; set; }
         public DateTime FechaIngreso { get; set; }
         public DateTime? FechaDevolucion { get; set; }
-        public bool EstaEnUso { get; set; }
-        public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }
+        public bool EstaEnMantenimiento => FechaDevolucion == null;
     }
 }
