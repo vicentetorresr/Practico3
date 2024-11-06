f7877ce1ed561600f087f6444d99166d1594a5e8
Danirock666 <jorgepardo642@gmail.com>
2024-11-06 09:51:32 -0300

    Cambio de diseño WEB

--- Practico3/Migrations/20241104140528_listo.Designer.cs
+++ Practico3/Migrations/20241104140528_listo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Practico3.Data;
 
@@ -11,9 +12,10 @@ using Practico3.Data;
 namespace Practico3.Migrations
 {
     [DbContext(typeof(Contextt))]
-    partial class ContexttModelSnapshot : ModelSnapshot
+    [Migration("20241104140528_listo")]
+    partial class listo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Practico3/Migrations/20241104140528_listo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practico3.Migrations
+{
+    public partial class listo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaAsignacion",
+                table: "Asignaciones",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 4, 11, 5, 28, 470, DateTimeKind.Local).AddTicks(9739),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 10, 29, 23, 3, 59, 80, DateTimeKind.Local).AddTicks(3458));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaAsignacion",
+                table: "Asignaciones",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 10, 29, 23, 3, 59, 80, DateTimeKind.Local).AddTicks(3458),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 4, 11, 5, 28, 470, DateTimeKind.Local).AddTicks(9739));
+        }
+    }
+}
